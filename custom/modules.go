ce92cf8e595be2baaf21f02da9f877ed17a3c1a5
programmer2123 <jburgess46@gmail.com>
2017-02-15 14:46:39 -0600

    Added TLS support

--- custom/modules.go
+++ custom/modules.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+  _ "github.com/looplab/logspout-logstash"
+  _ "github.com/gliderlabs/logspout/transports/tls"
 	_ "github.com/gliderlabs/logspout/adapters/syslog"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
 )
